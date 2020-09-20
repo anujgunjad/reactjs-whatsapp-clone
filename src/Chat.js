994467ec53652fcfafa121626418ab7cf8b19d6c
anujgunjad <a.gunjad11@gmail.com>
2020-09-20 23:52:32 +0530

    Inital commit

---
+++ src/Chat.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+function Chat() {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Anuj Gunjad</span>
+          Hey Guys
+          <span className="chat__timestamp">Anuj Gunjad</span>
+        </p>
+      </div>
+
+      <div className="chat__footer"></div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZnh4s2-v0DPz3rtWKwouCxI9WY_xBYCQ",
+  authDomain: "whatsapp-clone-614d8.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-614d8.firebaseio.com",
+  projectId: "whatsapp-clone-614d8",
+  storageBucket: "whatsapp-clone-614d8.appspot.com",
+  messagingSenderId: "94632701753",
+  appId: "1:94632701753:web:1d874ffab40ffa6afd42e8",
+  measurementId: "G-L88ZQVSKKX",
+};
